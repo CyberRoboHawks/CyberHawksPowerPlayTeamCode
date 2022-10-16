ab732e6c54f9aa5206b2922d8ab17f9f8c0f41d7
iceman <danheitzmann@gmail.com>
2022-10-16 10:18:18 -0400

    Update object detection for testing

--- VisionTest.java
+++ VisionTest.java
@@ -1,16 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Thread.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Vision Test", group="Pushbot")
-@Disabled
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+import java.util.List;
+
+@Autonomous(name = "Vision Test", group = "Pushbot")
+//@Disabled
 public class VisionTest extends LinearOpMode {
     ObjectDetection objectDetection = new ObjectDetection();
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
@@ -23,22 +29,33 @@ public class VisionTest extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
+        // run until the end of Autonomous
         while (opModeIsActive()) {
-            float duckPosition = objectDetection.getDuckPosition();
-            int state = objectDetection.getDuckState(duckPosition);
 
-            for (int i = 0; i<= 60; i++){
-                duckPosition = objectDetection.getDuckPosition();
-                state = objectDetection.getDuckState(duckPosition);
-                telemetry.addData("count ", i);
-                telemetry.addData("state ", state);
-                telemetry.addData("duck location", duckPosition);
-                telemetry.update();
-                sleep(500);
-            }
+            //getAllRecognitions();
+
+            PowerPlayEnums.parkingZone parkingZone = objectDetection.readParkingZone(5, 500);
+            telemetry.addData("parking location found", parkingZone);
+            telemetry.update();
 
             sleep(30000);
+        }
+    }
+
+
+    private void getAllRecognitions() {
+        List<Recognition> objects;
+        for (int i = 0; i <= 600; i++) {
+            objects = objectDetection.readCamera();
+            telemetry.addData("count", objects.size());
+            if (objects != null || !objects.isEmpty()) {
+                for (Recognition recognition : objects) {
+                    telemetry.addData("label", recognition.getLabel());
+                    telemetry.addData("confidence", recognition.getConfidence());
+                }
+            }
+            telemetry.update();
+            sleep(500);
         }
     }
 }
